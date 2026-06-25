bbb7f32b2f987e8ff7aaeabee09bb7be6abff1d9
Kunalsahuji <ksahu0103@gmail.com>
2026-06-25 18:44:56 +0530

    update mongo_uri

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -8,7 +8,7 @@ import Manager from '../models/Manager.js';
 export const protect = async (req, res, next) => {
   try {
     let token;
-
+//
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
     }
